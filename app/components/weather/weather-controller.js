2838ff79c0c87e681bf94ff57accee2a6dc0d54b
jordanengstrom <jordan.engstrom@gmail.com>
2018-02-18 19:06:01 -0700

    adding styles

--- app/components/weather/weather-controller.js
+++ app/components/weather/weather-controller.js
@@ -7,24 +7,23 @@ function WeatherController(){
 	function draw(temp,min,max,city) {
 		var weatherElem = document.getElementById('weather');
 		var template = `
+		<div id="weather-shape">
 		<div class="row">
-			<div class="col-sm-4">
+			<div class="col-sm-6">
 				<i class="fas fa-sun fa-3x"></i>
 			</div>
-			<div class="col-sm-4">
-				<h4>${city}</h4>
-			</div>
-			<div class="col-sm-4">
-				<h4>${temp}</h4>
+			<div class="col-sm-6">
+					<h4>${city}: ${temp}&#176 F</h4>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-sm-6">
-				<h6>Low: ${min}</h6>
+				<h6>Low: ${min}&#176 F</h6>
 			</div>
-			<div class="col-sm-6">
-				<h6>High: ${max}</h6>
-			</div>			
+				<div class="col-sm-6">
+					<h6>High: ${max}&#176 F</h6>
+				</div>
+		</div>
 		</div>
 		`
 		weatherElem.innerHTML = template;
